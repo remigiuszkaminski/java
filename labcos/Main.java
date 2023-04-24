b53633565772062890cd1134d3ec7ed8324ef118
Remigiusz Kamiński <50remek50@gmail.com>
2023-04-24 13:08:45 +0200

    oceny

---
+++ labcos/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Jest to program do operacji na array.");
+        GradeList nowy = new GradeList();
+        int opcja;
+        boolean program = true;
+        while(program == true) {
+            System.out.println("Wybierz jedna z opcji: \n" + "1 - Dodaj nowa ocene \n" + "2 - Policz srednia \n" + "3 - Podaj najwyzsza ocene \n" + "4 - Podaj najnizsza ocene \n" + "5 - Wyjdz z programu \n" );
+            opcja = sc.nextInt();
+            switch (opcja) {
+                case 1:
+                    System.out.println("Podaj nowa ocene:");
+                    double nowaocena = sc.nextDouble();
+                    nowy.addOcena(nowaocena);
+                    break;
+                case 2:
+                    double srednia = nowy.sredniaOcen();
+                    if(srednia == 0) System.out.println("Błąd, brak ocen w liście");
+                    else System.out.println("Srednia ocen wynosi : " + srednia);
+                    break;
+                case 3:
+                    double najwyzszy = nowy.najwyzszaOcena();
+                    if(najwyzszy == 0) System.out.println("Błąd, brak ocen w liście");
+                    else System.out.println("Najwyzsza ocena wynosi : " + najwyzszy);
+                    break;
+                case 4:
+                    double najnizszy = nowy.najnizszaOcena();
+                    if(najnizszy == 0) System.out.println("Błąd, brak ocen w liście");
+                    else System.out.println("Najnizsza ocena wynosi : " + najnizszy);
+                    break;
+                case 5:
+                    System.out.println("Koniec programu \n");
+                    sc.close();
+                    program = false;
+                    break;
+                case default:
+                    System.out.println("Zla opcja \n");
+                    break;
+                }
+            }
+
+
+    }
+}
+
+
---
+++ labcos/Walec.java
@@ -0,0 +1,46 @@
+// public class Walec {
+    
+//     private double podstawa;
+//     private double wysokosc;
+
+//     public double getPodstawa() {
+//         return podstawa;
+//     }
+//     public double getWysokosc() {// import java.lang.Math;
+//         return wysokosc;
+//     }
+
+//     public void setPodstawa( double newPodstawa) {
+//         this.podstawa = newPodstawa;
+//     }
+//     public void setWysokosc( double newWysokosc) {
+//         this.wysokosc = newWysokosc;
+//     }
+
+//     public Walec(double pod, double wys) {
+//         this.podstawa = pod;
+//         this.wysokosc = wys;
+//     }
+
+//     public Walec() {
+        
+//     }
+
+//     public double liczPolePod() {
+//         return Math.PI * Math.pow(podstawa, 2);
+//     }
+
+//     public double boczne() {
+//         return 2 * Math.PI * podstawa * wysokosc;
+//     }
+
+//     public double calkowite() {
+//         return 2 * liczPolePod() + boczne();
+//     }
+    
+//     public double objetosc() {
+//         return liczPolePod() * wysokosc;
+//     }
+
+
+// }
